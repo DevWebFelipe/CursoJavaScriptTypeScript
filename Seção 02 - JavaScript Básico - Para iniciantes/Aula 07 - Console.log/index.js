38548c11929f9c54a9f4d5c1b208fbea9f218264
Felipe Teixeira <109564897+DevWebFelipe@users.noreply.github.com>
2022-08-14 19:28:30 -0300

    Arquivos aula 07

---
+++ Seção 02 - JavaScript Básico - Para iniciantes/Aula 07 - Console.log/index.js
@@ -0,0 +1,17 @@
+console.log('Felipe Teixeira');
+console.log("Felipe Teixeira");
+console.log(`Felipe Teixeira`);
+
+//caso queira usar aspas simples dentro do texto, então as aspas de fora deverão ser aspas duplas
+console.log("Felipe 'Teixeira'");
+
+//caso queira usar aspas duplas dentro do texto, então as aspas de fora deverão ser aspas simples
+console.log('Felipe "Teixeira"');
+
+/*com crase pode-se usar tanto aspas simples quanto aspas duplas, pois nenhuma regra estará sendo
+violada, além disso, a crase também é usada para TEMPLATE STRINGS*/
+console.log(`'Felipe' "Teixeira"`);
+
+console.log(123456, 54546);
+console.log(123.45);
+console.log(23.10, 7.07, 'Felipe Teixeira');
