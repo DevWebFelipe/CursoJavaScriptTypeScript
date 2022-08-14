8e594bb51f382f7902bd61abf87b488d412d1c74
Felipe Teixeira <109564897+DevWebFelipe@users.noreply.github.com>
2022-08-14 19:36:44 -0300

    Arquivos aula 08

---
+++ Seção 02 - JavaScript Básico - Para iniciantes/Aula 08 - Exercícios/index.js
@@ -0,0 +1,5 @@
+//criar a frase da aula e exibir no console.log, exibir a hora como número, não como texto
+console.log('Meu nome é Felipe "Teixeira". Estou aprendendo JavaScript às', 10, 'da manhã.');
+
+//exemplos da aula (a vírgula concatena as sentenças, só que diferente do delphi, não precisa fazer cast pra tipagem de sentenças)
+console.log('Este', 'é', 'um', 1000);
