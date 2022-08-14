66cbe278512f832a619dc2ef61d15411dd31a8c0
Felipe Teixeira <109564897+DevWebFelipe@users.noreply.github.com>
2022-08-14 20:36:02 -0300

    Arquivos aula 11

---
+++ Seção 02 - JavaScript Básico - Para iniciantes/Aula 11 - Comentários de código/index.js
@@ -0,0 +1,24 @@
+
+//comentários de 1 linha
+console.log('ola mundo'); //aqui tem outro comentário
+/**
+ * Comentário com destaque
+ * cada nova linha
+ * automaticamente fica com 
+ * um asterísco 
+ */
+console.log('Linha 1');
+//outro comentário
+
+// console.log('Linha 2');
+
+console.log('Linha 3'); /*Comentários de bloco, também podem ser usados em uma linha só*/
+/*
+Comentário de bloco, para comentários longos
+*/
+console.log('Linha 4');
+console.log('Linha 5');
+/*console.log('Linha 6');
+  console.log('Linha 7');
+  console.log('Linha 8');*/
+//vai ser ignorado pelo motor do javascript
