63637110c85c8885558ed560f0fcee0b3a184bec
Felipe Teixeira <109564897+DevWebFelipe@users.noreply.github.com>
2022-08-14 22:43:10 -0300

    Arquivos aula 018

---
+++ Seção 02 - JavaScript Básico - Para iniciantes/Aula 018 - Let vs Var - Primeira diferença/index.js
@@ -0,0 +1,15 @@
+//ECMAScript = é uma padronização da liguagem javascript criada pela empresa ECMA International
+//ECMASCript 2015 na verdade é o (ES6) que é a versão
+
+//VAR - permite ser redeclarada e sobrescreve a declaração anterior
+//LET - não permite se redeclarada
+
+var nome = 'felipe';
+var nome = 'isis';
+
+console.log(nome);
+
+//cirar uma variável sem declarar se é var ou const ou let cria uma variável global
+sobrenome = 'teixeira';  //NÃO USAR DESSA FORMA
+
+console.log(sobrenome);
