2512e36aa298593616a0236aacc861e3fbdc5e32
Felipe Teixeira <109564897+DevWebFelipe@users.noreply.github.com>
2022-08-16 22:15:56 -0300

    Arquivos aula 21

---
+++ Seção 02 - JavaScript Básico - Para iniciantes/Aula 021 - Alert, Confirm e Prompt (Navegador)/assets/js/index.js
@@ -0,0 +1,11 @@
+alert('Mensagem de alerta');
+
+var mTeste = confirm('Deseja realmente excluir o registro?');
+
+console.log(mTeste);
+
+var mTeste = prompt('Digite seu nome');
+
+alert(mTeste);
+
+alert(prompt('Digite algo para a segunda mensagem'))
