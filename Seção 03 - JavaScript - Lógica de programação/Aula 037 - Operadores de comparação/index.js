06ba8fcf742baf3a24bec50748719ccba68f656d
Felipe Teixeira <109564897+DevWebFelipe@users.noreply.github.com>
2022-08-23 18:50:50 -0300

    Arquivos aula 037

---
+++ Seção 03 - JavaScript - Lógica de programação/Aula 037 - Operadores de comparação/index.js
@@ -0,0 +1,37 @@
+/*
+
+>      Maior que
+>=     Maior ou Igual
+<      Menor que
+<=     Menor ou Igual
+==     Igual (= Atribuição, cuidar para não confundir) - Checa se o valor é igual
+===    Igual estrito                                   - Checa se o valor é igual e se são do mesmo tipo
+!=     Diferente                                       - Checa se o valor é diferente
+!==    Diferente estrito                               - Checa se o valor é diferente e se o tipo é diferente
+*/
+
+/*    > Maior que    */
+console.log('10 maior que 5?', 10 > 5);
+
+/*    => Maior ou igual    */
+console.log('10 maior ou igual a 5?', 10 >= 5);
+
+/*    < Menor que    */
+console.log('10 menor que 5?', 10 < 5);
+
+/*    <= Menor ou igual    */
+console.log('10 menor ou igual a 5?', 10 <= 5);
+
+/*    == igual    */
+console.log('10 igual a 5?', 10 == 5);
+console.log('10 tipo string igual a 10 tipo integer?', '10' == 10);
+
+/*    === Igual e mesmo tipo    */
+console.log('10 tipo string igual a 10 tipo integer?', '10' === 10);
+
+/*    != Diferente    */
+console.log('10 tipo string diferente de 10 tipo integer?', '10' != 10);
+
+/*    !== Diferente e tipos diferentes    */
+console.log('10 tipo string diferente de 10 tipo integer?', '10' !== 10);
+console.log('10 tipo string diferente de 10 tipo integer?', '10' !== 5);
