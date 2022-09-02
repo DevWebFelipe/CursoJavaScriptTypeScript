ea4ebb977cad206ef3e9717feaedfb0863dab319
Felipe Teixeira <109564897+DevWebFelipe@users.noreply.github.com>
2022-09-01 23:31:46 -0300

    Conclusão configuração editor

---
+++ TypeScript/dist/index.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+function greet(name) {
+    return `Olá ${name}`;
+}
+console.log(greet(`Felipe Texeira`));
+exports.default = greet;
+const nome = `luiz`;
+console.log(nome);
