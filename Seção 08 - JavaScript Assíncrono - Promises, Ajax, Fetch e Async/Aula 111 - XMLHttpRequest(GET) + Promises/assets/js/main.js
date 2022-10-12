c61ace13b0a0bb1b1fdf6aaf47f557792b99af1f
Felipe Teixeira <109564897+DevWebFelipe@users.noreply.github.com>
2022-10-12 19:49:00 -0300

    Aula de XMLHttpRequest

---
+++ Seção 08 - JavaScript Assíncrono - Promises, Ajax, Fetch e Async/Aula 111 - XMLHttpRequest(GET) + Promises/assets/js/main.js
@@ -0,0 +1,13 @@
+/*
+const request = obj => {
+  //xhr === XML HTTP REQUEST
+  const xhr = new XMLHttpRequest();
+}*/
+
+
+function soma(x, y) {
+  return x + y
+}
+
+let total = soma(10, 20)
+console.log('Total =', total)
