44009c286f9d6f964514f8cb4e85a815a4fd8460
Felipe Teixeira <109564897+DevWebFelipe@users.noreply.github.com>
2022-11-01 21:10:30 -0300

    arquivos aula 116

---
+++ Seção 09 - JavaScript Tooling e ES6 Modules - Módulos/Aula 116 - Webpack (Boilerplate)/public/assets/js/bundle.js
@@ -0,0 +1,14 @@
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+alert('teste');
+
+//linha1
+//linha2
+
+console.log('teste');
+/******/ })()
+;
+//# sourceMappingURL=bundle.js.map
