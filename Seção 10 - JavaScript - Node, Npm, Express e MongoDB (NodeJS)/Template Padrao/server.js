a5728f46784a49fc386440a03dc6ce8e8af38753
Felipe Teixeira <109564897+DevWebFelipe@users.noreply.github.com>
2023-01-31 19:57:32 -0300

    Alterações BoilerPlate

--- Seção 10 - JavaScript - Node, Npm, Express e MongoDB (NodeJS)/Template Padrao/server.js
+++ Seção 10 - JavaScript - Node, Npm, Express e MongoDB (NodeJS)/Template Padrao/server.js
@@ -2,8 +2,8 @@ require('dotenv').config();
 
 const express = require('express');
 const app = express();
-
 const mongoose = require('mongoose');
+
 mongoose.set('strictQuery', true);
 mongoose.connect(process.env.CONECTIONSTRING, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => {
@@ -11,14 +11,30 @@ mongoose.connect(process.env.CONECTIONSTRING, { useNewUrlParser: true, useUnifie
   })
   .catch(e => console.log(e));
 
+const session = require('express-session');
+const MongoStore = require('connect-mongo');
+const flash = require('connect-flash');
+
 const routes = require('./routes');
 const path = require('path');
-const meuMiddleware = require('./src/middleware/middleware');
 const {middlewareGlobal} = require('./src/middleware/middleware');
 
 app.use(express.urlencoded({extended: true}));
 app.use(express.static(path.resolve(__dirname, 'public')));
-app.use(meuMiddleware);
+
+const sessionOptions = session({
+  secret: 'akasdfj0út23453456+54qt23qv  qwf qwer qwer qewr asdasdasda a6()',
+  store: MongoStore.create({ mongoUrl: process.env.CONECTIONSTRING }),
+  resave: false,
+  saveUninitialized: false,
+  cookie: {
+    maxAge: 1000 * 60 * 60 * 24 * 7, //tempo em milésimos de sgundos que a sessão vai durar
+    httpOnly: true
+  }
+});
+app.use(sessionOptions);
+app.use(flash());
+
 app.set('views', path.resolve(__dirname, 'src', 'views'));
 app.set('view engine', 'ejs');
 app.use(routes);
