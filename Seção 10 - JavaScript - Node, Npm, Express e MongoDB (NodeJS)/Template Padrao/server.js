a58f1228ba0145c40e7cbfb879aacce379012839
Felipe Teixeira <109564897+DevWebFelipe@users.noreply.github.com>
2023-01-30 21:27:05 -0300

    Alterações no template padrão

--- Seção 10 - JavaScript - Node, Npm, Express e MongoDB (NodeJS)/Template Padrao/server.js
+++ Seção 10 - JavaScript - Node, Npm, Express e MongoDB (NodeJS)/Template Padrao/server.js
@@ -1,5 +1,16 @@
+require('dotenv').config();
+
 const express = require('express');
 const app = express();
+
+const mongoose = require('mongoose');
+mongoose.set('strictQuery', true);
+mongoose.connect(process.env.CONECTIONSTRING, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    app.emit('Banco de dados conectado');
+  })
+  .catch(e => console.log(e));
+
 const routes = require('./routes');
 const path = require('path');
 const meuMiddleware = require('./src/middleware/middleware');
@@ -12,8 +23,10 @@ app.set('views', path.resolve(__dirname, 'src', 'views'));
 app.set('view engine', 'ejs');
 app.use(routes);
 
-const portaServidor = 3000;
-app.listen(portaServidor, () => {
-  console.log('Acessar servidor local http://127.0.0.1:' + portaServidor);
-  console.log('servidor rodando na porta: ' + portaServidor);
+app.on('Banco de dados conectado', () => {
+  const portaServidor = 3000;
+  app.listen(portaServidor, () => {
+    console.log('Acessar servidor local http://127.0.0.1:' + portaServidor);
+    console.log('servidor rodando na porta: ' + portaServidor);
+  });
 });
---
+++ Seção 10 - JavaScript - Node, Npm, Express e MongoDB (NodeJS)/Template Padrao/src/models/HomeModel.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+
+const HomeSchema = new mongoose.Schema({
+  titulo: {type: String, required: true},
+  id: String,
+  descricao: String,
+})
+
+const HomeModel = mongoose.model('Home', HomeSchema);
+
+class Home {
+
+};
+
+module.exports = HomeModel; //depois mudar por Home
