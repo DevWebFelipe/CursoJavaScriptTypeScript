4e99bf082aaccb858bdf4a7f551f628b19a906dc
Felipe Teixeira <109564897+DevWebFelipe@users.noreply.github.com>
2023-02-01 22:09:08 -0300

    Aula 146

--- Seção 11 - JavaScript Node - Projeto Agenda com Express e MongoDB/Projeto Agenda/src/controllers/userController.js
+++
@@ -1,9 +0,0 @@
-exports.paginaIncial = (req, res) => {
-  res.send(`
-  <form action="/" method="POST"> 
-  Usuário: <input type="text" name="nome">
-  Login: <input type="text" name="idade">
-  <button>Enviar usuário</button>
-  </form>
-  `)
-};
