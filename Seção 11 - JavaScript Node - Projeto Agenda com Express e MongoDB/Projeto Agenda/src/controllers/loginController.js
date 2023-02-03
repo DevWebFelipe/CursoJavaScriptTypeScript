1cac203fe0ecaeff9fb255b36e5b909765d8e441
Felipe Teixeira <109564897+DevWebFelipe@users.noreply.github.com>
2023-02-02 22:41:17 -0300

    Aula 149

--- Seção 11 - JavaScript Node - Projeto Agenda com Express e MongoDB/Projeto Agenda/src/controllers/loginController.js
+++ Seção 11 - JavaScript Node - Projeto Agenda com Express e MongoDB/Projeto Agenda/src/controllers/loginController.js
@@ -1,7 +1,9 @@
 const Login = require('../models/LoginModel');
 
 exports.index = (req, res) => {
-  res.render('login');
+  //console.log(req.session.user)
+  if(req.session.user) return res.render('login-logado')
+  return res.render('login');
 };
 
 exports.register = async function(req, res) {
@@ -13,6 +15,7 @@ exports.register = async function(req, res) {
       req.flash('errors', login.errors);
       req.session.save(function() {
         return res.redirect('/login/index');
+        //return res.redirect('back');
       });
       return;
     }
@@ -20,9 +23,41 @@ exports.register = async function(req, res) {
     req.flash('success', 'Seu usuário foi criado com sucesso.');
     req.session.save(function() {
       return res.redirect('/login/index');
+      //return res.redirect('back');
     });
   } catch(e) {
     console.log(e);
     return res.render('404');
   }
 };
+
+exports.login = async function(req, res) {
+  try {
+    const login = new Login(req.body);
+    await login.login();
+
+    if(login.errors.length > 0) {
+      req.flash('errors', login.errors);
+      req.session.save(function() {
+        return res.redirect('/login/index');
+        //return res.redirect('back');
+      });
+      return;
+    }
+
+    req.flash('success', 'Login efetuado com sucesso!');
+    req.session.user = login.user;
+    req.session.save(function() {
+      return res.redirect('/login/index');
+      //return res.redirect('back');
+    });
+  } catch(e) {
+    console.log(e);
+    return res.render('404');
+  }
+};
+
+exports.logout = function(req, res) {
+  req.session.destroy();
+  res.redirect('/');
+}
