6b5658cd79a7341d725b9f238f5b3f5981235e67
Felipe Teixeira <109564897+DevWebFelipe@users.noreply.github.com>
2023-02-02 22:42:05 -0300

    Aula 149

---
+++ Seção 11 - JavaScript Node - Projeto Agenda com Express e MongoDB/Aula 149 - Logando o usuário/routes.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const route = express.Router();
+
+const homeController = require('./src/controllers/homeController');
+const loginController = require('./src/controllers/loginController');
+
+// Rotas da home
+route.get('/', homeController.index);
+
+// Rodas do login
+route.get('/login/index', loginController.index);
+route.post('/login/register', loginController.register);
+route.post('/login/login', loginController.login);
+route.get('/login/logout', loginController.logout);
+
+module.exports = route;
