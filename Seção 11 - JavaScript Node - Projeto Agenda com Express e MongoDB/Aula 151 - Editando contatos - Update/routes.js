13135e927ca05489e10310ec9c3e186d2c036d1f
Felipe Teixeira <109564897+DevWebFelipe@users.noreply.github.com>
2023-02-03 18:24:52 -0300

    Aula 151

---
+++ Seção 11 - JavaScript Node - Projeto Agenda com Express e MongoDB/Aula 151 - Editando contatos - Update/routes.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const route = express.Router();
+
+const homeController = require('./src/controllers/homeController');
+const loginController = require('./src/controllers/loginController');
+const contatoController = require('./src/controllers/contatoController');
+
+const { loginRequired } = require('./src/middleware/middleware');
+
+// Rotas da home
+route.get('/', homeController.index);
+
+// Rodas do login
+route.get('/login/index', loginController.index);
+route.post('/login/register', loginController.register);
+route.post('/login/login', loginController.login);
+route.get('/login/logout', loginController.logout);
+
+// Rotas de contato
+route.get('/contato/index', loginRequired, contatoController.index);
+route.post('/contato/register', contatoController.register);
+route.get('/contato/index/:id', contatoController.editIndex);
+route.post('/contato/edit/:id', contatoController.edit);
+
+module.exports = route;
---
+++ Seção 11 - JavaScript Node - Projeto Agenda com Express e MongoDB/Aula 151 - Editando contatos - Update/webpack.config.js
@@ -0,0 +1,30 @@
+const path = require('path'); 
+
+module.exports = {
+  mode: 'development',
+  //mode: 'production',
+  entry: './frontend/main.js',
+
+  output: {
+    path: path.resolve(__dirname, 'public', 'assets', 'js'),
+    filename: 'bundle.js'
+  },
+
+  module: {
+    rules: [{
+      exclude: /node_modules/,
+      test: /\.js$/,
+      use: {
+        loader: 'babel-loader',
+        options: {
+          presets: ['@babel/env']
+        }
+      }
+    }, {
+      test: /\.css$/,
+      use: ['style-loader', 'css-loader']
+    }]
+  },
+
+  devtool: 'source-map'
+};
