75f190e6e52f052f104a6a8cf5f9216bc249bf16
Felipe Texeira <development.felipe@gmail.com>
2023-02-08 22:37:02 -0300

    Conclusão configuração do banco

--- Seção 13 - API REST - JavaScript Node Express e SQL com MySQL ou MariaDB/Projeto API REST/src/controllers/HomeController.js
+++ Seção 13 - API REST - JavaScript Node Express e SQL com MySQL ou MariaDB/Projeto API REST/src/controllers/HomeController.js
@@ -1,7 +1,18 @@
+import Aluno from '../models/Aluno';
+
 class HomeController {
-  index(req, res) {
+  async index(req, res) {
+    const novoAluno = await Aluno.create({
+      nome: 'Isis',
+      sobrenome: 'Gabrielle Texeira',
+      email: 'isis@gmail.com',
+      idade: 0,
+      peso: 7.0,
+      altura: 0.48,
+    });
+
     res.json({
-      tudoFuncionando: true,
+      novoAluno,
     });
   }
 }
