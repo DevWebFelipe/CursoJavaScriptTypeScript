c2dab1c04d71a80bb2fc4fd0183f6826c11db91d
Felipe Texeira <development.felipe@gmail.com>
2023-02-09 19:00:57 -0300

    Aula 162

--- Seção 13 - API REST - JavaScript Node Express e SQL com MySQL ou MariaDB/Projeto API REST/src/controllers/UserController.js
+++ Seção 13 - API REST - JavaScript Node Express e SQL com MySQL ou MariaDB/Projeto API REST/src/controllers/UserController.js
@@ -4,15 +4,66 @@ class UserController {
   async store(req, res) {
     try {
       const novoUser = await User.create(req.body);
-      res.json({
-        novoUser,
-      });
+      return res.json({ novoUser });
     } catch (e) {
-      res.status(400).json({
+      return res.status(400).json({
         errors: e.errors.map((err) => err.message),
       });
     }
   }
+
+  async index(req, res) {
+    try {
+      const users = await User.findAll();
+      return res.json(users);
+    } catch (e) {
+      return res.status(400).json({
+        errors: e.errors.map((err) => err.message),
+      });
+    }
+  }
+
+  async show(req, res) {
+    try {
+      const { id } = req.params;
+
+      const user = await User.findByPk(id);
+      return res.json(user);
+    } catch (e) {
+      return res.status(400).json({
+        errors: e.errors.map((err) => err.message),
+      });
+    }
+  }
+
+  // Update
+  async update(req, res) {
+    try {
+      if (!req.params.id) {
+        return res.status(400).json({
+          errors: ['Nenhum código de usuário enviado para busca!'],
+        });
+      }
+
+      const user = await User.findByPk(req.params.id);
+
+      if (!user) {
+        return res.status(400).json({
+          errors: ['Usuário não contrado!'],
+        });
+      }
+
+      const novosDados = await user.update(req.body);
+
+      return res.json(novosDados);
+    } catch (e) {
+      return res.status(400).json({
+        errors: e.errors.map((err) => err.message),
+      });
+    }
+  }
+
+  // Delete
 }
 
 export default new UserController();
--- Seção 13 - API REST - JavaScript Node Express e SQL com MySQL ou MariaDB/Projeto API REST/src/routes/userRoutes.js
+++ Seção 13 - API REST - JavaScript Node Express e SQL com MySQL ou MariaDB/Projeto API REST/src/routes/userRoutes.js
@@ -4,6 +4,9 @@ import userController from '../controllers/UserController';
 const router = new Router();
 
 router.post('/', userController.store);
+router.get('/', userController.index);
+router.get('/:id', userController.show);
+router.put('/:id', userController.update);
 
 export default router;
 
