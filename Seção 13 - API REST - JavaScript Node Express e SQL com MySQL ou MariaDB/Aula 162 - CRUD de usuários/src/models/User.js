1bd78e2a0dcba4963cba4a9081d51fd572b7270a
Felipe Texeira <development.felipe@gmail.com>
2023-02-09 19:07:42 -0300

    Aula 162

---
+++ Seção 13 - API REST - JavaScript Node Express e SQL com MySQL ou MariaDB/Aula 162 - CRUD de usuários/src/models/User.js
@@ -0,0 +1,58 @@
+import Sequelize, { Model } from 'sequelize';
+import bcryptjs from 'bcryptjs';
+
+export default class User extends Model {
+  static init(sequelize) {
+    super.init(
+      {
+        nome: {
+          type: Sequelize.STRING,
+          defaultValue: '',
+          validate: {
+            len: {
+              args: [4, 255],
+              msg: 'Nome precisa conter de 3 a 255 caracteres',
+            },
+          },
+        },
+        email: {
+          type: Sequelize.STRING,
+          defaultValue: '',
+          unique: {
+            msg: 'Email já existe!',
+          },
+          validate: {
+            isEmail: {
+              msg: 'E-mail inválido',
+            },
+          },
+        },
+        password_hash: {
+          type: Sequelize.STRING,
+          defaultValue: '',
+        },
+        password: {
+          type: Sequelize.VIRTUAL,
+          defaultValue: '',
+          validate: {
+            len: {
+              args: [6, 255],
+              msg: 'Senha precisa conter de 6 a 50 caracteres',
+            },
+          },
+        },
+      },
+      {
+        sequelize,
+      },
+    );
+
+    this.addHook('beforeSave', async (user) => {
+      if (user.password) {
+        user.password_hash = await bcryptjs.hash(user.password, 8);
+      }
+    });
+
+    return this;
+  }
+}
