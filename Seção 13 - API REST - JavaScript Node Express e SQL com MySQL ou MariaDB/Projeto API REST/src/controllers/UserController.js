2bc2231643085be799dbf900668ff2f4f176b463
Felipe Texeira <development.felipe@gmail.com>
2023-02-09 19:07:14 -0300

    Aula 162

--- Seção 13 - API REST - JavaScript Node Express e SQL com MySQL ou MariaDB/Projeto API REST/src/controllers/UserController.js
+++ Seção 13 - API REST - JavaScript Node Express e SQL com MySQL ou MariaDB/Projeto API REST/src/controllers/UserController.js
@@ -64,6 +64,31 @@ class UserController {
   }
 
   // Delete
+  async delete(req, res) {
+    try {
+      if (!req.params.id) {
+        return res.status(400).json({
+          errors: ['Nenhum código de usuário enviado para busca!'],
+        });
+      }
+
+      const user = await User.findByPk(req.params.id);
+
+      if (!user) {
+        return res.status(400).json({
+          errors: ['Usuário não contrado!'],
+        });
+      }
+
+      await user.destroy();
+
+      return res.json(user);
+    } catch (e) {
+      return res.status(400).json({
+        errors: e.errors.map((err) => err.message),
+      });
+    }
+  }
 }
 
 export default new UserController();
