adbb1eb1dbe288228bbab2a69bb1a5777785387d
Felipe Texeira <development.felipe@gmail.com>
2023-02-12 22:51:13 -0300

    Removido comemtários

--- Seção 13 - API REST - JavaScript Node Express e SQL com MySQL ou MariaDB/Projeto API REST/src/routes/tokenRoutes.js
+++ Seção 13 - API REST - JavaScript Node Express e SQL com MySQL ou MariaDB/Projeto API REST/src/routes/tokenRoutes.js
@@ -6,13 +6,3 @@ const router = new Router();
 router.post('/', tokenController.store);
 
 export default router;
-
-/*
- cada controler, pode ter até 5 métodos, não é uma regra, está mais para um padrão assim
-
- index GET -> Lista todos os registros
- store/create POST -> Cria um novo registro
- delete DELETE -> Apaga um registro
- show GET -> Mostra um registro
- update PACTH ou PUT -> Altera um registro
-*/
--- Seção 13 - API REST - JavaScript Node Express e SQL com MySQL ou MariaDB/Projeto API REST/src/routes/userRoutes.js
+++ Seção 13 - API REST - JavaScript Node Express e SQL com MySQL ou MariaDB/Projeto API REST/src/routes/userRoutes.js
@@ -14,16 +14,3 @@ router.put('/', loginRequired, userController.update);
 router.delete('/', loginRequired, userController.delete);
 
 export default router;
-
-/*
- cada controler, pode ter até 5 métodos, não é uma regra, está mais para um padrão assim
-
- index GET -> Lista todos os registros
- store/create POST -> Cria um novo registro
- delete DELETE -> Apaga um registro
- show GET -> Mostra um registro
- update PATCH ou PUT -> Altera um registro
-
- PATCH -> Normalmente usado para editar um campo apenas
- PUT -> Normalmente usado para editar o registro inteiro
-*/
